b7acd99d8cc25bb2808970d59f76e3a43aac7042
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-02-09 09:01:16 +0600

    corrects in ng-repeat

--- backend/modules/angular/views/index/ng-repeat.php
+++ backend/modules/angular/views/index/ng-repeat.php
@@ -19,18 +19,71 @@ AngularAsset::register($this);
     </h1>
     <div class="booksRepeatDiv" ng-controller="booksRepeatCtrl" url="<?php echo Url::to('/angular/index/ng-repeat'); ?>">
         <table class="table table-hover table-condensed table-striped table-bordered">
-            <tr>
-                <td>ID</td>
-                <td>name</td>
-                <td>author</td>
-                <td>publish date</td>
-            </tr>
-            <tbody ng-repeat="book in books">
+            <thead>
                 <tr>
+                    <td>ID</td>
+                    <td>name</td>
+                    <td>author</td>
+                    <td>publish date</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr ng-repeat="book in books">
                     <td>{{book.id}}</td>
                     <td>{{book.name}}</td>
                     <td>{{book.author_id}}</td>
                     <td>{{book.publish_date}}</td>
+                </tr>
+                <!--это один из самых распространенных способов использования ng-repeat -
+                генерация строк таблицы на основе коллекции данных-->
+                <!--источником данных может быть что-то перечислимое например массив или объект-->
+                <!--итерируя элементами массив angularjs создает экземпляр каждого item и
+                на этом экземпляре можно получить доступ к непосредственно самому объекту-->
+            </tbody>
+        </table>
+    </div>
+    <pre>
+        шаблонные директивы используются для того чтобы генерировать html по шаблону,
+        облегчить работу с коллекциями данных и для добавления базовой логики на страницу
+
+        генерация однотипных элементов самая распространенная задача в разработке и
+        для этого следует использовать директиву ng-repeat
+    </pre>
+    <div id="tasksPanel" class="panel" ng-controller="tasksRepeatCtrl">
+        <h3 class="panel-header">Tasks List</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Task</th>
+                    <th>Done</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- этот цикл перебирает каждый элемент массива и генерирует для него tr -->
+                <tr ng-repeat="task in tasks">
+                    <!-- этот перебирает каждое свойство в объекте и генерирует для него td,
+                    в котором используется one-way binding для отображения данных -->
+                    <td ng-repeat="property in task">{{property}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div id="tasksPanel" class="panel" ng-controller="tasksRepeatCtrl">
+        <h3 class="panel-header">Tasks List</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Task</th>
+                    <th>Done</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr ng-repeat="task in tasks">
+                    <td ng-repeat="(key, value) in task">
+                        <!--при таком использовании первое значение всегда указывает на ключ,
+                        а второе непосредственно на само значение-->
+                        {{key}} - {{value}}
+                    </td>
                 </tr>
             </tbody>
         </table>
--- backend/web/angular/ngRepeat.js
+++ backend/web/angular/ngRepeat.js
@@ -23,4 +23,19 @@ function ngRepeat()
             console.log(status);
         });
     });
+
+
+    app.controller("tasksRepeatCtrl", function ($scope) {
+            $scope.tasks = [
+                { action: "Buy flowers", complete: false },
+                { action: "Go to gym", complete: false },
+                { action: "Buy snickers", complete: true },
+                { action: "Buy notebook", complete: false },
+                { action: "Call friends", complete: false },
+                { action: "Go to cinema", complete: false },
+                { action: "Buy Big-Mac", complete: true },
+                { action: "Buy a book", complete: false },
+                { action: "Call mom", complete: false }
+            ];
+        });
 }
