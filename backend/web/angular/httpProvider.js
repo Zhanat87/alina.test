d6c17930c77cc92441d0b4186af1560dabea154c
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-02-12 16:35:46 +0600

    last changes

--- backend/web/angular/httpProvider.js
+++ backend/web/angular/httpProvider.js
@@ -7,16 +7,27 @@ function httpProvider()
     app.config(function ($httpProvider) {
         $httpProvider.defaults.transformResponse.push(function (responseData, headers) {
             fruits = [];
-            var fruitElems = angular.element(responseData.trim()).find("fruit");
+            var fruitElems = responseData;
             for (var i = 0; i < fruitElems.length; i++) {
-                var fruit = fruitElems.eq(i);
+                var fruit = fruitElems[i];
                 fruits.push({
-                    name: fruit.attr("name"),
-                    category: fruit.attr("category"),
-                    price: fruit.attr("price")
+                    name: fruit["name"] + ' test',
+                    price: fruit["price"] + 123
                 });
             }
+            //console.log(responseData);
             return fruits;
+            //fruits = [];
+            //var fruitElems = angular.element(responseData).find("fruit");
+            //for (var i = 0; i < fruitElems.length; i++) {
+            //    var fruit = fruitElems.eq(i);
+            //    fruits.push({
+            //        name: fruit.attr("name") + ' test',
+            //        //category: fruit.attr("category"),
+            //        price: fruit.attr("price") + 123
+            //    });
+            //}
+            //return fruits;
         })
     }).controller("httpProviderCtrl", function($scope, $http) {
         $scope.getFruits = function () {
@@ -30,6 +41,7 @@ function httpProvider()
         $httpProvider.interceptors.push(function () {
             return {
                 request: function (config) {
+                    console.log(config)
                     config.url = "/angular/ajax/data.json";
                     return config;
                 },
