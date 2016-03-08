2c3a3ae0c498665c927e109b9dd44ddf59611e52
Iskakov Zhanat <iskakov.zhanat@gmail.com>
2016-03-08 10:22:09 +0600

    frontend\assets\AngularAsset

---
+++ frontend/assets/AngularAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanat
+ * Date: 3/7/16
+ * Time: 10:08 PM
+ */
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+use yii\web\View;
+
+class AngularAsset extends AssetBundle
+{
+
+    public $sourcePath = '@bower';
+
+    public $js = [
+        'angular/angular.js',
+        'angular-route/angular-route.js',
+        'angular-strap/dist/angular-strap.js',
+    ];
+
+    public $jsOptions = [
+        'position' => View::POS_HEAD,
+    ];
+
+}
